91043c4d27b7400c9444cce8e57b72c4400a28bf
Swordholder <925937463@qq.com>
2019-11-28 20:28:19 +0800

    写完了元文件读取部分

--- service/resources/task_metadata.py
+++ service/resources/task_metadata.py
@@ -7,6 +7,7 @@ class TaskMetaData(Resource):
 
     def post(self, user_id, task_id):
         task_type = request.form['task_type']
+        dataset_name = request.form['dataset_name']
         meta_file_location = os.path.join(config['UPLOAD_FOLDER'], user_id, task_id)
         meta = {}
 
@@ -15,8 +16,9 @@ class TaskMetaData(Resource):
             meta['task_type'] = task_type
 
             dataset_info = {}
-            dataset_info['name'] = self.get_dataset_name()
-            samples_info = self.get_samples_info()
+            dataset_info['name'] = dataset_name
+            # get_samples_info返回一个由'suze'、'num'、'class'组成的字典的列表
+            samples_info = self.get_samples_info(os.path.join(meta_file_location, dataset_name))
             dataset_info['size'] = sum([s['size'] for s in samples_info])
             dataset_info['nums'] = [s['num'] for s in samples_info]
             dataset_info['sample_num'] = sum(dataset_info['nums'])
@@ -29,3 +31,14 @@ class TaskMetaData(Resource):
 
         return meta, 200
 
+    def get_samples_info(self, dataset_root):
+        '''
+            返回一个由{'size': xx, 'num': xx, 'class': xx}组成的列表
+            其中，size指的是子文件夹大小，num指的是样本数量，class指的是样本类别（即子文件名）
+        '''
+        sample_names = os.listdir(dataset_root)
+        sample_size = [os.path.getsize(os.path.join(dataset_root, sample)) for sample in sample_names]
+        sample_num = [self.file_counts(os.path.join(dataset_root, sample)) for sample in sample_names]
+    
+    def file_counts(self, path):
+        return len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
